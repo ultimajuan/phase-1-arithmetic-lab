6f75584cb0491d74f51fea47a3998a3b041008ee
Juan <juancollazonyc@gmail.com>
2021-06-30 12:09:26 -0400

    commit tree f471de61482d71843e4e9e4823bc19aa1588ec80

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
 
-// Write your code here
+let random = Math.floor(Math.random() * 4) + 1;
+
+let num3 = 8;
+let num4 = 2;
+
+let mod = num3 / num4;
+
+let max = Math.max(1,3, 20);
